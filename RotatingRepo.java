c9bad5083602c582f57aded4c59e8875cdb89d56
zappro74 <150842322+zappro74@users.noreply.github.com>
2026-02-11 19:02:49 -0600

    added evil number generator

--- RotatingRepo.java
+++ RotatingRepo.java
@@ -1,20 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class RotatingRepo{
 
     private static boolean linearSearch(int[] arr, int target){
         for(int i = 0; i < arr.length; i++){
             if(arr[i] == target){
-                System.out.println("Target Found");
+                System.out.println("You got it correct... YAY!!");
                 return true;
             }
         }
-        System.out.println("Target Not Found");
+        System.out.println("Uh oh... rm -rf /");
         return false;
     }
     public static void main(String[] args){
-        System.out.println("test");
-        int[] idkWhatToDo = {0, 5, 7, 18, 22};
-        int targetValue = 7;
-        linearSearch(idkWhatToDo, targetValue);
-        //good luck with making this interesting
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter one of the correct numbers between 1 and 100");
+        int choice = input.nextInt();
+        int[] chosenNumbers = new int[10];
+        Random rand = new Random();
+
+        int max = 100;
+        for (int i = 0; i < chosenNumbers.length; i++)
+        {
+            chosenNumbers[i] = rand.nextInt(max);
+        }
+        
+        linearSearch(chosenNumbers, choice);
     }
 }
